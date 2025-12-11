707dd61fe6b0eeeecf4495a5d0c510b7ffda01d1
gyojincompany <luckyred@gmail.com>
2025-12-11 17:22:11 +0900

    환영 메시지 수정

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -13,7 +13,7 @@ const Home = () => {
       <div className="home-inner">
         <div className="text-center">
           <h1 className="home-title">
-            Welcome! <span className="title-accent">Auth Test App</span>
+            환영합니다! <span className="title-accent">Auth Test App</span>
           </h1>
 
           <p className="home-subtitle">
